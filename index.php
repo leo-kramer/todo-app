2ceb8159e40ff4e2cbc8770caeadfdc0ebed8962
leo-kramer <leokramer17@gmail.com>
2024-12-15 14:01:12 +0100

    Apply new task styling

--- index.php
+++ index.php
@@ -61,8 +61,8 @@
                 </ul>
               </div>
             </div>
-            <button class='btn' onclick='deleteTask($index)'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+            <button class='btn p-2' onclick='deleteTask($index)'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
               <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>
               <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>
             </svg>
@@ -73,23 +73,26 @@
 
       // Create a task
       echo "</ul>
-        <form method='post' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "'>
-          <button type='submit' name='submit'>
-          <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus' viewBox='0 0 16 16'>
-            <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4'/>
-          </svg>
-          </button>
+        <form class='d-flex flex-column justify-content-center' method='post' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "'>
+          <div class='d-flex mb-2 p-2 rounded'>
+            <button class='btn text-bg-dark p-0 d-flex align-items-center' type='submit' name='submit'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-plus' viewBox='0 0 16 16'>
+              <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4'/>
+            </svg>
+            </button>
 
-          <input type='text' name='task' placeholder='New task' required>
+            <input class='flex-fill text-bg-dark' type='text' name='task' placeholder='New task' required>
+          </div>
+
           <input type='hidden' name='status' value='$status'>
-
-          <label for='priority'>Priority: </label>
-          <select name='priority' required>
-            <option value='Immediate'>Immediate</option>
-            <option value='High'>High</option>
-            <option value='Normal' selected>Normal</option> <!-- Default option -->
-            <option value='Low'>Low</option>
-          </select>
+          <div class='btn-group'>
+            <select class='form-select-sm dropdown-toggle' name='priority' required>
+              <option value='Immediate'>Immediate</option>
+              <option value='High'>High</option>
+              <option value='Normal' selected>Normal</option>
+              <option value='Low'>Low</option>
+            </select>
+          </div>
         </form>
       </section>";
     }
